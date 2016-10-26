62001bf1376cf461ae2674d4d42c9665dbcf05ad
unknown <>
2016-10-26 18:51:42 +0800

    10.25

---
+++ src/yawnlon/algorithms/P405.java
@@ -0,0 +1,20 @@
+package yawnlon.algorithms;
+
+public class P405 {
+
+	public static void main(String[] args) {
+		System.out.println(toHex(-1));
+	}
+
+	public static String toHex(int num) {
+		char[] map = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		if (num == 0)
+			return "0";
+		String result = "";
+		while (num != 0) {
+			result = map[num & 15] + result;
+			num = (num >>> 4);
+		}
+		return result;
+	}
+}
